66e5266dff04f1755de5833c74497f6461c7c054
Tucker Fredrickson <tuck.fredrickson@gmail.com>
2016-09-28 17:34:53 -0700

    re-master

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 
 $(document).ready(function(){
-
-	//generates random number
-  	var computerNumber = 1 + Math.floor(Math.random() * 100);
-  	console.log(computerNumber);
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -16,17 +12,71 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	$("#guessButton").on('click', function(){
-  		console.log("clicked");
+    //generates random number
+    var computerNumber = 1 + Math.floor(Math.random() * 100);
+    console.log(computerNumber);
+  	
+    //Hot or cold Function
+    $("#guessButton").on('click', function(e){
   		var userNumber = $("#userGuess").val();
-  		console.log(userNumber);
-  		if (userNumber == computerNumber) {
-  			alert("You got it!");
+  		 if (userNumber == "") {
+        alert("Please enter a number.")
+        e.preventDefault();
+        return false;
+
+       }
+       else if (userNumber == computerNumber) {
+        alert("That's the number!!");
+        e.preventDefault();
+        return false;
+      }
+
+      else if (userNumber - computerNumber <= 10 && userNumber - computerNumber >= 0-10) {
+  			alert("You're Super HOT!!!");
+        e.preventDefault();
+        return false;
   		}
+
+      else if (userNumber - computerNumber <= 20 && userNumber - computerNumber >= 0-20) {
+        alert("You're Hot :)!!");
+        e.preventDefault();
+        return false;
+      }
+
+      else if (userNumber -  computerNumber <= 30 && userNumber - computerNumber >= 0-30) {
+        alert("You're getting warm!");
+        e.preventDefault();
+        return false;
+      }
+
+      else if (userNumber - computerNumber <= 50 && userNumber - computerNumber >= 0-50) {
+        alert("You're cold :(");
+        e.preventDefault();
+        return false;
+      }
+
   		else {
-  			alert("you didn't get it :(");
-  		}
+  			alert("You're very far away...");
+        e.preventDefault();
+        return false;
+  		};
   	});
+
+    //counts clicks on submit button
+    $("#guessButton").click(function(){
+      $("#count").html(function(i, val){
+        return +val+1;
+      });
+    });
+
+    $("#guessButton").click(function(){
+      $(".text").value = " ";
+    });
+
+    //reloads page creating new game
+    $("#newGame").click(function(){
+      location.reload();
+    });
 });
 
 
